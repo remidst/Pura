823c3c2cd6c2887cf3779b375eff74455cd7ab72
remidst <remidaste@hotmail.fr>
2017-08-06 11:27:43 +0200

    updated the projects controller and show page to display all messages belonging to the project

--- Beta/app/controllers/projects_controller.rb
+++ Beta/app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @project=Project.find(params[:id])
+    @messages=@project.messages
   end
 
   # GET /projects/new
