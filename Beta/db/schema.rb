334fbacab2f9cb6654554541e444935667e2ba98
remidst <remidaste@hotmail.fr>
2017-08-06 22:14:31 +0200

    created the migration to add foreign key user_id to messages

--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806200846) do
+ActiveRecord::Schema.define(version: 20170806201306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,9 @@ ActiveRecord::Schema.define(version: 20170806200846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "project_id"
+    t.bigint "user_id"
     t.index ["project_id"], name: "index_messages_on_project_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -67,5 +69,6 @@ ActiveRecord::Schema.define(version: 20170806200846) do
 
   add_foreign_key "documents", "projects"
   add_foreign_key "messages", "projects"
+  add_foreign_key "messages", "users"
   add_foreign_key "projects", "users"
 end
