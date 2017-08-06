eb4d6f5802e4f3d93fcc5b11a4fd683c46eb838c
remidst <remidaste@hotmail.fr>
2017-08-07 00:31:27 +0200

    updated the messages controller and model to retrieve user id

--- Beta/app/controllers/messages_controller.rb
+++ Beta/app/controllers/messages_controller.rb
@@ -4,7 +4,10 @@ class MessagesController < ApplicationController
 
   def create
     @project = Project.find(params[:project_id])
+    @user = current_user
     @message = @project.messages.create(message_params)
+    @message.set_user!(current_user)
+
     redirect_to project_path(@project), notice: "The message has been sent."
   end
 
@@ -19,7 +22,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :user)
   end
 
 end
--- Beta/app/models/message.rb
+++ Beta/app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
 	belongs_to :project
 	belongs_to :user
+
+	def set_user!(user)
+		self.user_id = user.id
+		self.save!
+	end
 end
