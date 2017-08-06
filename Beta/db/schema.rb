08fd6017da4d0e8ec288e79e8b5bd19be4788a15
remidst <remidaste@hotmail.fr>
2017-08-06 10:25:11 +0200

    modified the project and message model to create has many/belongs to association

--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805224806) do
+ActiveRecord::Schema.define(version: 20170806081930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "project_id"
+    t.index ["project_id"], name: "index_messages_on_project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170805224806) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "projects"
 end
