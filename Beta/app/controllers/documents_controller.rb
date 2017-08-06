3f34f5be292cdcc1a6b87b04f154dc82b1996003
remidst <remidaste@hotmail.fr>
2017-08-07 00:30:48 +0200

    updated the documents controller and model to retrieve user id

--- Beta/app/controllers/documents_controller.rb
+++ Beta/app/controllers/documents_controller.rb
@@ -1,7 +1,9 @@
 class DocumentsController < ApplicationController
   def create
       @project = Project.find(params[:project_id])
+      @user = current_user
       @document = @project.documents.create(document_params)
+      @document.set_user!(current_user)
       redirect_to project_path(@project), notice: "The document has been uploaded."
   end
 
@@ -10,7 +12,7 @@ class DocumentsController < ApplicationController
 
   private
   def document_params
-  	params.require(:document).permit(:attachment)
+  	params.require(:document).permit(:attachment, :user)
   end
 
 end
--- Beta/app/models/document.rb
+++ Beta/app/models/document.rb
@@ -2,4 +2,10 @@ class Document < ApplicationRecord
 	belongs_to :project
 	belongs_to :user
 	mount_uploader :attachment, AttachmentUploader #Tells rails to use this uploader for the documents model	
+
+	def set_user!(user)
+		self.user_id = user.id
+		self.save!
+	end
+
 end
