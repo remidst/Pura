799852515750a2e036e4dfbd7ac8513713c78743
remidst <remidaste@hotmail.fr>
2017-08-06 14:21:20 +0200

    error, went back to original project model with just the name

--- Beta/db/migrate/20170806101500_fix_column_name.rb
+++
@@ -1,8 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[5.1]
-  def self.up
-  	rename_column :projects, :name, :first_name
-  end
-
-  def self.down
-  end
-end
--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806101500) do
+ActiveRecord::Schema.define(version: 20170806081930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20170806101500) do
     t.string "first_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name"
   end
 
   create_table "users", force: :cascade do |t|
