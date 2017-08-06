094711d36d6ea74b1c7bd40d81633960af941820
remidst <remidaste@hotmail.fr>
2017-08-06 14:24:22 +0200

    renamed column for projects model

--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806081930) do
+ActiveRecord::Schema.define(version: 20170806122220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170806081930) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "first_name"
+    t.string "project_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
