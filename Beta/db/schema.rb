708b109b0ac61c173006dd4b69db71aa5c919804
remidst <remidaste@hotmail.fr>
2017-08-06 15:32:29 +0200

    Created the Document model and migrated it

--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806122220) do
+ActiveRecord::Schema.define(version: 20170806133011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "documents", force: :cascade do |t|
+    t.string "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
