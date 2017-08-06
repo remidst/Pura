c0a26b039c84944003525f539083039301acf098
remidst <remidaste@hotmail.fr>
2017-08-06 21:41:07 +0200

    updated the models to link project to user

--- Beta/app/models/project.rb
+++ Beta/app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+	has_many :users, through: :memberships
+	has_one :leader, class_name: 'User', foreign_key: 'user_id'
 	has_many :messages
 	has_many :documents
 end
--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806134732) do
+ActiveRecord::Schema.define(version: 20170806170407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170806134732) do
     t.datetime "updated_at", null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_documents_on_project_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_memberships_on_project_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
