ec0143a7b7fd8c011e62041833e5de5fb2f9c758
remidst <remidaste@hotmail.fr>
2017-08-06 15:57:30 +0200

    wrote create method in documents controller

--- Beta/app/controllers/documents_controller.rb
+++ Beta/app/controllers/documents_controller.rb
@@ -1,7 +1,16 @@
 class DocumentsController < ApplicationController
   def create
+      @project = Project.find(params[:project_id])
+      @document = @project.document.create(document_params)
+      redirect_to project_path(@project), notice: "The document has been uploaded."
   end
 
   def destroy
   end
+
+  private
+  def document_params
+  	params.require(:document).permit(:attachment)
+  end
+
 end
