c9fc171b6c30584367b4a9ebb1ddfde4a3ece6e5
remidst <remidaste@hotmail.fr>
2017-08-09 11:15:47 +0200

    reverted a migration that prevented heroku to deploy

--- Beta/db/migrate/20170806122220_fix_column_name.rb
+++
@@ -1,9 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[5.1]
-  def self.up
-    rename_column :projects, :first_name, :project_name
-  end
-
-  def self.down
-    # rename back if you need or do something else or do nothing
-  end
-end
