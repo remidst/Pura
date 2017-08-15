0e58f39af7d0d5e87db83732a7464373b4101581
remidst <remidaste@hotmail.fr>
2017-08-15 09:45:54 +0200

    added dependecies in project model, to cascade destroy messages, documents and membership

--- Beta/app/models/project.rb
+++ Beta/app/models/project.rb
@@ -1,10 +1,10 @@
 class Project < ApplicationRecord
-	has_many :memberships
+	has_many :memberships, dependent: :destroy
 	has_many :users, through: :memberships
 
 	has_one :leader, class_name: 'User', foreign_key: 'leader_id'
-	has_many :messages
-	has_many :documents
+	has_many :messages, dependent: :destroy
+	has_many :documents, dependent: :destroy
 
   def leader
     self.memberships.where(type: 'leader').first
