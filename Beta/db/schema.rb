a02e30904caa76aacba43ca79886eed2560da6ef
remidst <remidaste@hotmail.fr>
2017-08-16 10:28:25 +0200

    migrated username to db

--- Beta/db/schema.rb
+++ Beta/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809092019) do
+ActiveRecord::Schema.define(version: 20170816082232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170809092019) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
