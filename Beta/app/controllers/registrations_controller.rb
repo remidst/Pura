ec10ae65ab819c921e303feffb0f0ef74b404bb6
remidst <remidaste@hotmail.fr>
2017-08-16 17:29:56 +0200

    added the company field to the user model and corrected some errors in pundit views

--- Beta/app/controllers/registrations_controller.rb
+++ Beta/app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
-class RegistrationsController < Devise::RegistrationsController
+	class RegistrationsController < Devise::RegistrationsController
 
-  private
+	  private
 
-  def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
-  end
+	  def sign_up_params
+	    params.require(:user).permit(:username, :company, :email, :password, :password_confirmation)
+	  end
 
-  def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
-  end
-end
+	  def account_update_params
+	    params.require(:user).permit(:username, :company, :email, :password, :password_confirmation, :current_password)
+	  end
+	end
