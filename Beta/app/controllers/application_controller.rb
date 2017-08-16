ddd4da8f625f90612d778b7570b4a9eede78630a
remidst <remidaste@hotmail.fr>
2017-08-16 18:04:36 +0200

    sign in with only username and company name

--- Beta/app/controllers/application_controller.rb
+++ Beta/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -11,4 +12,11 @@ class ApplicationController < ActionController::Base
 	    flash[:alert] = "You are not authorized to perform this action"
 	    redirect_to(request.referrer || root_path)
 	end
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :company, :password])
+	end
+
 end
