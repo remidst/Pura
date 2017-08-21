3e9ff6faccae38c48b4e261c9ba05e00f261e09a
remidst <remidaste@hotmail.fr>
2017-08-21 09:30:02 +0200

    created route for user index and created controller user

---
+++ Beta/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+	def index
+		@users=User.order(:username)
+		respond_to do |format|
+			format.html
+			format.json { render json: @users.where("username like ?", "%#{params[:q]}%") }
+	end
+
+end
--- Beta/config/routes.rb
+++ Beta/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
   get 'documents/create'
 
   get 'documents/destroy'
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => {registrations: 'registrations'}
+  resources :users, only: [:index]
 
   root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
