0ca1444e4daebe940fecee750f9d04938eb38ba9
remidst <remidaste@hotmail.fr>
2017-08-22 02:34:33 +0200

    fail

--- Beta/config/environments/production.rb
+++ Beta/config/environments/production.rb
@@ -32,10 +32,6 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
- config.assets.debug = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
