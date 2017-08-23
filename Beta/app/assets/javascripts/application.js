76c954fad6783a4f6e521a0070f1c21387f8ea34
remidst <remidaste@hotmail.fr>
2017-08-23 18:22:04 +0200

    fail

--- Beta/app/assets/javascripts/application.js
+++ Beta/app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery3
+//= require rails-ujs
 //= require jquery_ujs
 //= require rails-jquery-tokeninput
 //= require_tree .
