f6b4afb9f05710304ba3d048a6d91a6b60333d5e
remidst <remidaste@hotmail.fr>
2017-08-24 11:25:16 +0200

    added the validation to have only one same user per project

--- Beta/app/models/membership.rb
+++ Beta/app/models/membership.rb
@@ -2,6 +2,8 @@ class Membership < ApplicationRecord
 	belongs_to :user
 	belongs_to :project
 
+	validates_uniqueness_of :user_id, scope: :project_id
+
 
 	def set_project!(project)
 		self.project_id=project.id
