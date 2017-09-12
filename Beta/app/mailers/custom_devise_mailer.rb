8fdb10d10f0499e2a64f9cd91796c0aaef96c274
remidst <remidaste@hotmail.fr>
2017-09-12 11:28:39 +0200

    added custom name for invitation email

---
+++ Beta/app/mailers/custom_devise_mailer.rb
@@ -0,0 +1,10 @@
+class CustomDeviseMailer < Devise::Mailer
+protected
+
+	def subject_for(key)
+		return super unless key.to_s == 'invitation_instructions'
+
+		I18n.t('devise.mailer.invitation_instructions.subject'), :invited_by => "#{resource.invited_by.try(:username)}様から" )
+	end
+
+end
