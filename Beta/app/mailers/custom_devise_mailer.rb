cd131dc04f0de3829590eb8a0436d5b92d18d7dd
remidst <remidaste@hotmail.fr>
2017-09-13 18:09:24 +0200

    abandonned the custom email from devise invitable for now

--- Beta/app/mailers/custom_devise_mailer.rb
+++ Beta/app/mailers/custom_devise_mailer.rb
@@ -4,7 +4,7 @@ protected
 	def subject_for(key)
 		return super unless key.to_s == 'invitation_instructions'
 
-		I18n.t('devise.mailer.invitation_instructions.subject', :invited_by => "#{resource.invited_by.try(:username)}様から" )
+		I18n.t('devise.mailer.invitation_instructions.subject', :invited_by => resource.invited_by.try(:username) )
 	end
 
 end
