79af2d40f0469c8b9eca221b31425066c1659392
remidst <remidaste@hotmail.fr>
2017-09-30 11:29:43 +0200

    updated text for a mailer

--- Beta/app/mailers/user_mailer.rb
+++ Beta/app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 	def welcome(user)
 		@user = user
 
-		mail to: @user.email, subject: "#{@user.username}プラプラへようこそ！"
+		mail to: @user.email, subject: "#{@user.username}様、プラプラへようこそ！"
 	end
 end
