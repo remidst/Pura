774aaa3e1845d72cfc984c18a9a1bac3b949a6f6
remidst <remidaste@hotmail.fr>
2017-10-06 18:50:38 +0200

    deleted current user from project user list and tokeninput searchbar

--- Beta/app/controllers/users_controller.rb
+++ Beta/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users=User.order(:username)
+		@users=User.where.not(email: current_user.email).order(:username)
 		respond_to do |format|
 			format.html
 			format.json { render json: @users.where("UPPER(REPLACE(username, ' ','')) like UPPER(?)", "%#{params[:q]}%") }
