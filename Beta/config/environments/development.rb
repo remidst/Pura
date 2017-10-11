66f053f2e1d7b871f432b05141ce2becb6bae00a
remidst <remidaste@hotmail.fr>
2017-10-11 08:20:54 +0200

    mailer layouts

--- Beta/config/environments/development.rb
+++ Beta/config/environments/development.rb
@@ -31,7 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+
+  config.action_mailer.asset_host = 'https://s3.us-east-2.amazonaws.com/puraimage/'
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
