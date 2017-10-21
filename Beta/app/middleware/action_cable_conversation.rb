cab7bc0ce4f5ed2d0b4b9ef5d3a2977a42665773
remidst <remidaste@hotmail.fr>
2017-10-21 16:27:04 +0900

    created a middleware for production

---
+++ Beta/app/middleware/action_cable_conversation.rb
@@ -0,0 +1,14 @@
+class ConversationActionCable
+
+	def initialize(app, options={})
+		@app = app
+	end
+
+	def call(env)
+		if Faye::Websocket.websocket?(env)
+			ActionCable.server.call(env)
+		else
+			@app.call(env)
+		end
+	end
+end
