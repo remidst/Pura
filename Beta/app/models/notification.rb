a15fd9958fc0960242270adebc71b51aafadbd84
remidst <remidaste@hotmail.fr>
2017-10-30 01:00:07 +0900

    created the methods to generate text for notification

--- Beta/app/models/notification.rb
+++ Beta/app/models/notification.rb
@@ -6,4 +6,25 @@ class Notification < ApplicationRecord
   	self.content = "新しいファイルが共有されました。"
   	self.save!
   end
+
+  def new_message!
+  	self.content = "新しいメッセージが共有されました。"
+  	self.save!
+  end
+
+  def new_project!
+  	self.content = "新しい案件に招待されました。"
+  	self.save!
+  end
+
+  def new_user!
+  	self.content = "新しいメンバーが案件に招待されました。"
+  	self.save!
+  end
+
+  def new_leader!
+  	self.content = "担当ケアマネジャーが変更されました。"
+  	self.save!
+  end
+
 end
