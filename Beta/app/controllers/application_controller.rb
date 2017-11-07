334be323709d5109b0f58ecb87eed6ad3c0274a9
remidst <remidaste@hotmail.fr>
2017-11-07 14:16:40 +0900

    enabling authentication from url

--- Beta/app/controllers/application_controller.rb
+++ Beta/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user_from_token!
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
@@ -7,6 +8,15 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
 	private
+
+	def authenticate_user_from_token!
+		user_email = params[:user_email].presence
+		user = user_email && User.find_by_email(user_email)
+
+		if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+			sign_in user
+		end 
+	end
 
 	def user_not_authorized
 	    flash[:alert] = "この機能を使うアクセス権限がありません。"
