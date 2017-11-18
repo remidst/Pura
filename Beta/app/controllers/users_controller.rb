0264c1d998cd1ea6261612152521089f43fbbf78
remidst <remidaste@hotmail.fr>
2017-11-18 17:00:46 +0900

    cannot lookup deleted user in token

--- Beta/app/controllers/users_controller.rb
+++ Beta/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index
-		@users=User.where.not(email: current_user.email).order(:username)
+		@users=User.where(deleted_at: nil).where.not(email: current_user.email, username: [nil, ""]).order(:username)
 		respond_to do |format|
 			format.html
 			format.json { render json: @users.where("UPPER(REPLACE(username, ' ','')) like UPPER(?)", "%#{params[:q]}%") }
