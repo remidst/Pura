101d33564f40597da04466bd115a2eaefc34ae0c
remidst <remidaste@hotmail.fr>
2017-11-25 15:22:12 +0900

    new helpers and flow reviewed

--- Beta/app/helpers/contact_helper.rb
+++ Beta/app/helpers/contact_helper.rb
@@ -1,2 +1,25 @@
 module ContactHelper
+	def contact_name(contact)
+		if contact.care_manager_id.to_s == current_user.id.to_s
+			contact.service_provider.username.present? ? contact.service_provider.username : contact.service_provider.email
+		elsif contact.service_provider_id.to_s == current_user.id.to_s
+			contact.care_manager.username
+		end
+	end
+
+	def contact_company(contact)
+		if contact.care_manager_id.to_s == current_user.id.to_s
+			contact.service_provider.company.present? ? contact.service_provider.company : contact.service_provider.company
+		elsif contact.service_provider_id.to_s == current_user.id.to_s
+			contact.care_manager.company
+		end
+	end
+
+	def contact_role(contact)
+		if contact.care_manager_id.to_s == current_user.id.to_s
+			"責任者："
+		elsif contact.service_provider_id.to_s == current_user.id.to_s
+			"ケアマネージャー"
+		end
+	end
 end
--- Beta/app/models/reporting.rb
+++ Beta/app/models/reporting.rb
@@ -1,4 +1,5 @@
 class Reporting < ApplicationRecord
   belongs_to :contact
   has_many :reporting_attachments
+  accept_nested_attributes_for :reporting_attachments
 end
