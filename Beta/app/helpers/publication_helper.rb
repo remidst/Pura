b963c3ec3fde19b33c67eaa3490ae8c9e53ca24a
remidst <remidaste@hotmail.fr>
2018-02-04 23:27:18 +0100

    temporary version of the header helper

--- Beta/app/helpers/publication_helper.rb
+++ Beta/app/helpers/publication_helper.rb
@@ -1,2 +1,28 @@
 module PublicationHelper
+
+	def publication_header(publication)
+		if publication.publication_comments.present?
+			last_comment = publication.publication_comments.last
+				if last_comment.publisher == current_user
+					if publication.publisher == current_user
+						"#{publication.project.project_name.gsub(/\s+/, '')}様の案件に書いたコメント"
+					else
+						"#{publication.project.project_name.gsub(/\s+/, '')}様の案件の#{publication.publisher.username}様のメッセージに書いたコメント"
+					end
+				else
+					if publication.publisher == current_user
+						"#{last_comment.publisher.username}様が#{publication.project.project_name.gsub(/\s+/, '')}様の案件のメッセージにコメントを書きました"
+					else
+						"#{last_comment.publisher.username}様が#{publication.project.project_name.gsub(/\s+/, '')}様の案件のメッセージにコメントを書きました"
+					end
+				end
+		else
+			# no comments on this publication
+			if publication.publisher == current_user
+				"#{publication.project.project_name.gsub(/\s+/, '')}様の案件に共有したメッセージ"
+			else
+				"#{publication.publisher.username}様が#{publication.project.project_name.gsub(/\s+/, '')}様の案件にメッセージを共有しました"
+			end
+		end
+	end
 end
--- Beta/app/helpers/reportings_helper.rb
+++ Beta/app/helpers/reportings_helper.rb
@@ -1,2 +1,15 @@
 module ReportingsHelper
+
+	def reporting_header(reporting)
+		contact = reporting.contact
+		publisher = reporting.publisher
+		publisher == contact.care_manager ? receiver = contact.service_provider : receiver = contact.care_manager
+
+		if publisher == current_user
+			"#{receiver.username}様に送信した書類"
+		else
+			"#{receiver.username}様から書類が共有されました"
+		end
+	end
+
 end
