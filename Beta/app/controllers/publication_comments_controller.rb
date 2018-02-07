84c011a7a205eb55117d38bdb405552f7384f7a8
remidst <remidaste@hotmail.fr>
2018-02-07 15:43:31 +0100

    AJAX forms for comments

--- Beta/app/controllers/publication_comments_controller.rb
+++ Beta/app/controllers/publication_comments_controller.rb
@@ -1,23 +1,24 @@
 class PublicationCommentsController < ApplicationController
 
 	def create
-		publication = Publication.find(params[:publication_id])
-		publication_comment = publication.publication_comments.new(publication_comment_params)
-		publication_comment.publisher_id = current_user.id 
+		@publication = Publication.find(params[:publication_id])
+		@publication_comment = @publication.publication_comments.new(publication_comment_params)
+		@publication_comment.publisher_id = current_user.id 
 
 		respond_to do |format|
-			if publication_comment.save
+			if @publication_comment.save
 
 				#save all the comment's attachments
 				if params[:publication_comment_attachments].present?
 					params[:publication_comment_attachments]['attachment'].each do |a|
-						publication_comment_attachment = publication_comment.publication_comment_attachments.create!(attachment: a)
+						publication_comment_attachment = @publication_comment.publication_comment_attachments.create!(attachment: a)
 					end
 				end
 
-				format.html { redirect_to publication.project, notice: 'コメントが送られました。' }
+				format.html { redirect_to @publication.project, notice: 'コメントが送られました。' }
+				format.js
 			else
-				format.html { redirect_to publication.project, notice: 'コメントは送られませんでした。' }
+				format.html { redirect_to @publication.project, notice: 'コメントは送られませんでした。' }
 			end
 		end
 	end
