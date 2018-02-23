7248e450ab1c9c309b95f2aec697e9efadd0f56f
remidst <remidaste@hotmail.fr>
2018-02-23 01:39:12 +0100

    added japanese fonts

--- Beta/config/initializers/pdfkit.rb
+++ Beta/config/initializers/pdfkit.rb
@@ -16,6 +16,16 @@ PDFKit.configure do |config|
     :disable_smart_shrinking=> false
   }
 
+  if Rails.env.production?
+    font_dir = File.join(Dir.home, ".fonts")
+    Dir.mkdir(font_dir) unless Dir.exists?(font_dir)
+
+    Dir.glob(Rails.root.join("vendor", "fonts", "*")).each do |font|
+      target = File.join(font_dir, File.basename(font))
+      File.symlink(font, target) unless File.exists?(target)
+    end
+  end
+
 end
 
 
